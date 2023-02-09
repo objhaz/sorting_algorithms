78d96889818fa1ba2330964bfac81b547b7b36a6
objhaz <61233825+objhaz@users.noreply.github.com>
2023-02-09 21:29:13 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,47 +1,32 @@
 #include "sort.h"
 
 /**
- * swap_values - swaps 2 values in an array of ints
- *
- * @array: the array of ints
- * @i1: index of first value
- * @i2: index of 2nd value
- *
- * Return: the array with value
- */
-
-void swap_values(int **array, ssize_t i1, ssize_t i2)
-{
-	int tmp;
-
-	tmp = (*array)[i1];
-	(*array)[i1] = (*array)[i2];
-	(*array)[i2] = tmp;
-}
-
-/**
- * bubble_sort - sorts an array of integers using bubble sort
- *
- * @array: the array of integers
- * @size: the size of the array
+ * bubble_sort - function that sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ * @array: array of int to sort
+ * @size: size of the array
+ * Return: Nothing void
  */
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t n = size, new_n, i;
+	size_t cloop, cloop2;
+	int tmp;
 
-	while (n > 1)
+	if (array == NULL || size < 2)
+		return;
+
+	for (cloop2 = 1; cloop2 <= size; cloop2++)
 	{
-		new_n = 0;
-		for (i = 1; i <= n - 1; i++)
+		for (cloop = 0; cloop < size - 1; cloop++)
 		{
-			if (array[i - 1] > array[i])
+			if (array[cloop] > array[cloop + 1])
 			{
-				swap_values(&array, i - 1, i);
+				tmp = array[cloop];
+				array[cloop] = array[cloop + 1];
+				array[cloop + 1] = tmp;
 				print_array(array, size);
-				new_n = i;
 			}
 		}
-		n = new_n;
 	}
 }
